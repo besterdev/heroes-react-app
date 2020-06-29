186209f33f4570df189ba8854856b98e96ea5848
thawatchai <thawatchai.krai@gmail.com>
2020-06-29 22:22:57 +0700

    Updates

---
+++ precache-manifest.ca8ac545c3c91a17a02449eebdd25fee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6930b5c0f671042103a1674d931facb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64db09809390c3d481dc",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "64db09809390c3d481dc",
+    "url": "/static/js/2.e0ab4b4f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e0ab4b4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec55ba4f6c0f1ea53d6c",
+    "url": "/static/js/main.2533bbab.chunk.js"
+  },
+  {
+    "revision": "652a909d2c7614fc83c4",
+    "url": "/static/js/runtime-main.82c53c28.js"
+  }
+]);
